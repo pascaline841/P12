b14241fdc06dfd681ac078b5ae9c5558708db3f2
Pascaline M <pascaline.martres@gmail.com>
2021-10-12 16:01:37 -0400

    permission customers ok

--- epicevents/permissions.py
+++ epicevents/permissions.py
@@ -1,19 +1,30 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from users.models import User
 
 
 class IsAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ["PUT", "PATCH", "DELETE"]:
-            admin_users = User.objects.filter(role="ADMIN")
-            return request.user in admin_users.all()
+        admin_users = User.objects.filter(role="ADMIN")
+        if request.user in admin_users.all():
+            return request.method in [
+                "GET",
+                "PUT",
+                "PATCH",
+                "OPTIONS",
+                "HEAD",
+                "DELETE",
+            ]
+        else:
+            return request.method in SAFE_METHODS
 
 
 class IsSaler(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ["PUT", "PATCH"]:
-            saler_users = User.objects.filter(role="SALE")
-            return request.user in saler_users.all()
+        sale_users = User.objects.filter(role="SALE")
+        if request.user in sale_users.all():
+            return request.method in ["GET", "PUT", "PATCH", "OPTIONS", "HEAD"]
+        else:
+            return request.method in SAFE_METHODS
 
 
 class IsSalesContact(BasePermission):
