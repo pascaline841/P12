adcbb03f0350bf541e783cc5b1acf29c16a1df8a
Pascaline M <pascaline.martres@gmail.com>
2021-10-22 16:19:04 -0400

    connection api

--- epicevents/settings.py
+++ epicevents/settings.py
@@ -145,6 +145,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
